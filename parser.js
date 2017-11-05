897f36e8864145ee5b48d39c9407f4212098649f
Kamotenaut <skeith1002@gmail.com>
2017-11-06 02:11:33 +0800

    added seperate function to create each ui component, added inner text for each element

--- parser.js
+++ parser.js
@@ -54,58 +54,77 @@ function createUIControlElements() {
   })
 }
 
+
+
 function addUIControlButtion(tempUIControl) {
   $(document).ready(function() {
     console.log(tempUIControl);
     var $elem;
     switch (tempUIControl.type) {
       case "1":
-        console.log("Adding Button: " + tempUIControl.name);
-        $elem = $('<button/>', {
-          text: tempUIControl.text,
-          id: tempUIControl.name,
-          class: 'btn btn-primary'
-        });
-        $elem.css({
-          position: "absolute",
-          top: tempUIControl.y+"pt",
-          left: tempUIControl.x+"pt",
-          width: tempUIControl.width+"pt",
-          height: tempUIControl.height+"pt"
-        });
-
+        $elem = createUIControlButton(tempUIControl.text, tempUIControl.name, tempUIControl.y, tempUIControl.x, tempUIControl.width, tempUIControl.height);
         break;
       case "2":
-        $elem = $('<input/>', {
-          class: 'form-control',
-          type: 'text',
-          text: tempUIControl.text,
-          id: tempUIControl.name
-        });
-        $elem.css({
-          position: "absolute",
-          top: tempUIControl.y+"pt",
-          left: tempUIControl.x+"pt",
-          width: tempUIControl.width+"pt",
-          height: tempUIControl.height+"pt"
-        });
-        console.log("Adding Textbox: " + tempUIControl.name);
+        $elem = createUIControlTextBox(tempUIControl.text, tempUIControl.name, tempUIControl.y, tempUIControl.x, tempUIControl.width, tempUIControl.height);
         break;
       case "3":
-        $elem = $('<label/>', {
-          text: tempUIControl.text,
-          id: tempUIControl.name
-        });
-        $elem.css({
-          position: "absolute",
-          top: tempUIControl.y+"pt",
-          left: tempUIControl.x+"pt",
-          width: tempUIControl.width+"pt",
-          height: tempUIControl.height+"pt"
-        });
-        console.log("Adding Label: " + tempUIControl.name);
+        $elem = createUIControlLabel(tempUIControl.text, tempUIControl.name, tempUIControl.y, tempUIControl.x, tempUIControl.width, tempUIControl.height);
         break;
     }
     $('.container').append($elem);
   })
+}
+
+function createUIControlButton(textUI, nameUI, yUI, xUI, widthUI, heightUI) {
+  var $elem;
+  console.log("Adding Button: " + nameUI);
+  $elem = $('<button/>', {
+    text: textUI,
+    id: nameUI,
+    class: 'btn btn-primary'
+  });
+  $elem.css({
+    position: "absolute",
+    top: yUI + "pt",
+    left: xUI + "pt",
+    width: widthUI + "pt",
+    height: heightUI + "pt"
+  });
+  return $elem;
+}
+
+function createUIControlTextBox(textUI, nameUI, yUI, xUI, widthUI, heightUI) {
+  var $elem;
+  $elem = $('<input/>', {
+    class: 'form-control',
+    type: 'text',
+    text: textUI,
+    id: nameUI
+  });
+  $elem.css({
+    position: "absolute",
+    top: yUI + "pt",
+    left: xUI + "pt",
+    width: widthUI + "pt",
+    height: heightUI + "pt"
+  });
+  console.log("Adding Textbox: " + nameUI);
+  return $elem;
+}
+
+function createUIControlLabel(textUI, nameUI, yUI, xUI, widthUI, heightUI) {
+  var $elem;
+  $elem = $('<label/>', {
+    text: textUI,
+    id: nameUI
+  });
+  $elem.css({
+    position: "absolute",
+    top: yUI + "pt",
+    left: xUI + "pt",
+    width: widthUI + "pt",
+    height: heightUI + "pt"
+  });
+  console.log("Adding Label: " + nameUI);
+  return $elem;
 }
